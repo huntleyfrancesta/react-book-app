abe759f247c738e4ae7806dc3fe78d8137f7b384
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-06-26 01:56:47 -0400

    commit

--- models/book.js
+++ models/book.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    id: {type: String, require: true},
-    title: { type: String, require: true},
-    authors: [{ type: String, require: true}],
-    description: { type: String, require: true},
-    image: { type: String, unique: true, dropDups: true},
-    link: { type: String, required: true}
-})
+    _id: { type: String },
+    title: { type: String },
+    authors: { type: Array },
+    description: { type: String },
+    image: { type: String },
+    link: { type: String }
+});
 
-const GoogleBooks = mongoose.model("GoogleBooks", bookSchema);
+const Book = mongoose.model("Book", bookSchema);
 
-module.exports = GoogleBooks;
+module.exports = Book;
