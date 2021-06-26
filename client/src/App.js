2b60690230b65b15b8700e00f22b033be9f368de
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-06-26 02:04:54 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
 import Saved from "./pages/Saved";
+
 import "./App.css";
 
-
-
 class App extends Component {
-
-
+ 
   render() {
     return (
       <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/saved" component={Saved} />
-        </Switch>
-      </div>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
       </Router>
-    );
-  }
-}
+    )
+  };
+};
 
 export default App;
